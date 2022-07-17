140034ee476f7bbd4b730eb3614eba606ea629df
lbuday1975 <lbuday@mac.com>
2022-07-17 23:46:59 +0200

    add compute

---
+++ compute.py
@@ -0,0 +1,52 @@
+def get_vm_status(vm_name):
+    return "ONLINE"
+#    vm = _get_vm(vm_name)
+#    vm_rg = vm.id.split("/")[4]#
+
+#    client = _get_az_client()
+#    vm_instance = client.virtual_machines.instance_view(vm_rg, vm_name).as_dict()
+#    statuses = vm_instance["statuses"]
+#    try:
+#        state_dict = list(filter(lambda x: x["code"].startswith("PowerState"), statuses))[0]
+#        # e.g. {'code': 'PowerState/running', 'level': 'Info', 'display_status': 'VM running'}
+#        power = state_dict["code"].split("/")[1]
+#    except:
+#        power = "unknown"
+#    try:
+#        state_dict = list(filter(lambda x: x["code"].startswith("ProvisioningState"), statuses))[0]
+#        # e.g. {'code': 'PowerState/running', 'level': 'Info', 'display_status': 'VM running'}
+#        provisioning = state_dict["code"].split("/")[1]
+#    except:
+#        provisioning = "unknown"#
+#
+#    return {"power": power, "provisioning": provisioning}
+
+
+def vm_power(args, target_state):
+    vm = _get_vm(args.target)
+    vm_rg = vm.id.split("/")[4]
+    vm_status = get_vm_status(args.target)
+
+    client = _get_az_client()
+    if target_state == "on":
+        if vm_status["power"] == "running":
+            print("VM already in target state")
+            return
+        az_action = client.virtual_machines.begin_start(vm_rg, args.target)
+    elif target_state == "off":
+        if  vm_status["power"] == "deallocated":
+            print("VM already in target state")
+            return
+        az_action = client.virtual_machines.begin_deallocate(vm_rg, args.target)
+    elif target_state == "restart":
+        if  vm_status["power"] == "deallocated":
+            # Restart fails on not running machines...
+            # CloudError: Azure Error: OperationNotAllowed
+            # Message: The operation requires the VM to be running (or set to run).
+            az_action = client.virtual_machines.begin_start(vm_rg, args.target)
+        else:
+            az_action = client.virtual_machines.begin_restart(vm_rg, args.target)
+    else:
+        raise NotImplementedError("Action '%s' not implemented" % target_state)
+    az_action.wait()
+    print(az_action.status())
--- main.py
+++ main.py
@@ -1,14 +1,67 @@
-import requests;
+import requests
+import argparse
+import sys
+
+from .compute import vm_power, get_vm_status
+
+__version__ = '0.1.0'
+
 from requests.auth import HTTPBasicAuth
 
-GV_CONF_FILE="/opt/cloud/tenants/default/main.tfvars"
-GV_USER = ""
-GV_CRED = ""
+GV_CONF_FILE="/opt/cloud/tenants/default/credentials.tfvars"
+GV_USER = ''
+GV_CRED = ''
 
-def func_read_conf():
+GV_AVAILABLE_ACTIONS = [
+        'vm_start', 'vm_stop',
+        'vm_restart', 'vm_status'
+    ]
+
+GV_REQUIRED_PARAMETERS = {
+        "vm_start": ["target"],
+        "vm_stop": ["target"],
+        "vm_restart": ["target"],
+        "vm_status": ["target"]
+    }
+
+
+def checkParameters(args, parser):
+    if args.version:
+        return
+
+    if not "action" in args or not args.action:
+        raise parser.error("No action given.")
+
+    if args.action not in GV_AVAILABLE_ACTIONS:
+        raise parser.error("Invalid action.")
+
+    if args.action in GV_REQUIRED_PARAMETERS:
+        for parameter in GV_REQUIRED_PARAMETERS[args.action]:
+            if not parameter in args or getattr(args,parameter) in [None, ""]:
+                raise parser.error(f"Parameter --{parameter} is required for action '{args.action}', but has not been provided.")
+
+
+def func_pars_args():
+    print("PARSER")
+    LV_PARSER = argparse.ArgumentParser(description=f"LNW-Soft Infrastructure Connector SkyTap {__version__}")
+    LV_PARSER.add_argument("--version", action='store_true', help="Show version information")
+    LV_PARSER.add_argument("-a", "--action", choices=GV_AVAILABLE_ACTIONS)
+    LV_PARSER.add_argument("-t", "--target", help="[vm_*] Target VM")
+    LV_ARGS = LV_PARSER.parse_args()
+
+    checkParameters(LV_ARGS, LV_PARSER)
+
+    if args.version:
+        print(f"LNW-Soft Infrastructure Connector Azure {__version__}")
+        sys.exit()
+    elif args.action == "vm_start":
+        vm_power(args, "on")
+
+
+def func_read_conf_cred(FV_FILE):
     try:
         count = 0
-        LV_CONF = open(GV_CONF_FILE, 'r')
+        LV_CONF = open(FV_FILE, 'r')
         while True:
             count += 1
             LV_LINE = LV_CONF.readline()
@@ -16,29 +69,47 @@ def func_read_conf():
                 break
 
             if str.__contains__(LV_LINE, "="):
-                if str.__contains__(LV_LINE, 'skytap_conn_usr'):
-                    LV_VALUE = LV_LINE.split(sep='=')
-                    GV_USER = LV_VALUE[1].replace(' ', '').replace('\n', '')
                 if str.__contains__(LV_LINE, 'skytap_conn_cred'):
                     LV_VALUE = LV_LINE.split(sep='=')
-                    GV_CRED = LV_VALUE[1].replace(' ', '').replace('\n', '')
+                    GV_CRED = LV_VALUE[1].replace(' ', '').replace('\n', '').replace('"', '')
 
-        print(f'User {GV_USER} , passwd: {GV_CRED}')
         LV_CONF.close()
+        return GV_CRED
 
     except FileNotFoundError:
-        print("Config file not found or not readable. (/opt/cloud/tenants/default/main.tfvars)")
+        print("Config file not found or not readable. (/opt/cloud/tenants/default/credentials.tfvars)")
         exit(2)
 
+def func_read_conf_usr(FV_FILE):
+    try:
+        count = 0
+        LV_CONF = open(FV_FILE, 'r')
+        while True:
+            count += 1
+            LV_LINE = LV_CONF.readline()
+            if not LV_LINE:
+                break
+
+            if str.__contains__(LV_LINE, "="):
+                if str.__contains__(LV_LINE, 'skytap_conn_user'):
+                    LV_VALUE = LV_LINE.split(sep='=')
+                    GV_USER = LV_VALUE[1].replace(' ', '').replace('\n', '').replace('"', '')
+
+        LV_CONF.close()
+        return GV_USER
+
+    except FileNotFoundError:
+        print("Config file not found or not readable. (/opt/cloud/tenants/default/credentials.tfvars)")
+        exit(2)
 
 def func_get_status():
-    ##    api_url = "https://10.144.45.42/cloud.skytap.com/v2/configurations/127468724/vms.json?query=vm_name:SAPtest1"
 
     api_url = "https://cloud.skytap.com/v2/configurations.json"
 
-    response = requests.get(api_url, auth=HTTPBasicAuth('lbuday@lnwsoft.de_227d', 'ed1ac7dd1b4ca3ad1f312777cc7649064e2a2ec8'))
+    print("USR:" + GV_USER)
+    print("PWD:" + GV_CRED)
+    response = requests.get(api_url, auth=HTTPBasicAuth(GV_USER, GV_CRED))
     print("RC:" + str(response.status_code))
-    print(type(response.json()))
     LV_RESP=response.json()
 
     print("RET111:" + str(LV_RESP))
@@ -48,6 +119,8 @@ def func_get_status():
 
 
 if __name__ == '__main__':
-    func_read_conf()
+    func_pars_args()
+    GV_USER = func_read_conf_usr(GV_CONF_FILE)
+    GV_CRED = func_read_conf_cred(GV_CONF_FILE)
     func_get_status()
 
